b8f3b0a5ab88b476399f228acf3dd411240ddf6a
come2ry <raiki5100@gmail.com>
2022-01-08 18:59:59 +0900

    DELETE: print debug

--- Library/test/verify/aoj-1549_test.py
+++ Library/test/verify/aoj-1549_test.py
@@ -23,26 +23,19 @@ def main():
 
     q: int = int(input())
     for _ in range(q):
-        # print(f"i: {_}")
         l, r, d = map(int, input().split())
         r += 1
         d += OFS
         ans: int = OFS * 2
-        # print(f"wm.rank(d, l): {wm.rank(d, l)}")
-        # print(f"wm.rank(d, r): {wm.rank(d, r)}")
         if(wm.rank(d, l) < wm.rank(d, r)):
             ans = 0
         else:
             succ: int = wm.next_value(l, r, d)
-            # print(f"succ: {succ}")
             if(~succ):
                 ans, _ = chmin(ans, abs(succ - d))
-            # print(f"ans: {ans}")
             pred: int = wm.prev_value(l, r, d)
-            # print(f"pred: {pred}")
             if(~pred):
                 ans, _ = chmin(ans, abs(pred - d))
-            # print(f"ans: {ans}")
         print(ans)
 
 
