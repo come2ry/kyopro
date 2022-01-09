b78b24ba689a3d8e282dce61708ba8ea1835213a
come2ry <raiki5100@gmail.com>
2022-01-09 20:08:16 +0900

    AC: abc229_D

--- Contests/abc229/D/main.py
+++ Contests/abc229/D/main.py
@@ -1,24 +1,65 @@
 #!/usr/bin/env python3
+# https://atcoder.jp/contests/abc229/tasks/abc229_D?lang=ja
+
+import bisect
+import heapq
+import math
 import sys
+from collections import defaultdict, deque
+from itertools import (accumulate, groupby, permutations, combinations,
+                       combinations_with_replacement, product)
+from typing import *
+input = sys.stdin.buffer.readline
+
+
+T = TypeVar('T')
+D = TypeVar('D')
+
+
+def chmax(a: T, b: T) -> Tuple[T, bool]:
+    if (a < b):
+        a = b  # aをbで更新
+        return (a, True)
+    return (a, False)
+
+
+def chmin(a: T, b: T) -> Tuple[T, bool]:
+    if (a > b):
+        a = b  # aをbで更新
+        return (a, True)
+    return (a, False)
 
 
 def solve(S: str, K: int):
-    # cnt[r] - cnt[l] で s[l,r) の '.' の数を表す
-    cnt = [0]*(len(S)+1)
-    for i in range(len(S)):
-        if S[i] == '.':
-            cnt[i+1] = cnt[i] + 1
-        else:
-            cnt[i+1] = cnt[i]
-
-    r = 0
+    n = len(S)
+    a = [int(si == '.') for si in S]
     ans = 0
-    for l in range(len(S)):
-        while (r < len(S)) and (cnt[r+1] - cnt[l] <= K):
+    r, sum_ = 0, 0
+    for l in range(n):
+        while ((r < n) and (sum_ + a[r] <= K)):
+            sum_ += a[r]
             r += 1
-        # [l, r)に.がK超過したらr-lがlength
-        ans = max(r - l, ans)
+        ans, _ = chmax(ans, r-l)
+        sum_ -= a[l]
     print(ans)
+
+# def solve(S: str, K: int):
+#     # cnt[r] - cnt[l] で s[l,r) の '.' の数を表す
+#     cnt = [0]*(len(S)+1)
+#     for i in range(len(S)):
+#         if S[i] == '.':
+#             cnt[i+1] = cnt[i] + 1
+#         else:
+#             cnt[i+1] = cnt[i]
+
+#     r = 0
+#     ans = 0
+#     for l in range(len(S)):
+#         while (r < len(S)) and (cnt[r+1] - cnt[l] <= K):
+#             r += 1
+#         # [l, r)に.がK超過したらr-lがlength
+#         ans = max(r - l, ans)
+#     print(ans)
 
 
 def main():
