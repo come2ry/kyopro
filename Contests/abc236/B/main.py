f2f4f13f0dad0950aab2dbe307dd58f73b372c86
come2ry <raiki5100@gmail.com>
2022-01-24 00:32:51 +0900

    AC: abc236_B

---
+++ Contests/abc236/B/main.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# https://atcoder.jp/contests/abc236/tasks/abc236_B?lang=ja
+import bisect  # noqa: F401
+import heapq  # noqa: F401
+import math  # noqa: F401
+import sys  # noqa: F401
+from collections import defaultdict, deque  # noqa: F401
+from functools import lru_cache  # noqa: F401
+from itertools import (accumulate, combinations,  # noqa: F401
+                       combinations_with_replacement, groupby, permutations,
+                       product)
+from typing import *  # noqa: F401
+
+input = (lambda: sys.stdin.readline().rstrip("\r\n"))
+
+
+def chmax(a: Any, b: Any) -> Tuple[Any, bool]:
+    if (a < b):
+        a = b  # aをbで更新
+        return (a, True)
+    return (a, False)
+
+
+def chmin(a: Any, b: Any) -> Tuple[Any, bool]:
+    if (a > b):
+        a = b  # aをbで更新
+        return (a, True)
+    return (a, False)
+
+
+def solve(N: int, A: "List[int]"):
+    ans = [4] * N
+    for i in range(len(A)):
+        ans[A[i] - 1] -= 1
+
+    for i in range(N):
+        if ans[i] > 0:
+            print(i + 1)
+            break
+
+    return
+
+
+def main():
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+    tokens = iterate_tokens()
+    N = int(next(tokens))  # type: int
+    A = [int(next(tokens)) for _ in range(4 * N - 1)]  # type: "List[int]"
+    solve(N, A)
+
+
+if __name__ == '__main__':
+    main()
