8b6eabc13f5a56e3345b63fa4ff8d33da628987e
come2ry <raiki5100@gmail.com>
2022-02-19 20:26:13 +0900

    AC: abc237_C

---
+++ Contests/abc237/C/main.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# https://atcoder.jp/contests/abc237/tasks/abc237_C?lang=ja
+import bisect  # noqa: F401
+import heapq  # noqa: F401
+import math  # noqa: F401
+import sys  # noqa: F401
+from collections import defaultdict, deque  # noqa: F401
+from functools import lru_cache  # noqa: F401
+from itertools import (accumulate, combinations,  # noqa: F401
+                       combinations_with_replacement, groupby, permutations,
+                       product)
+from typing import *  # noqa: F401
+
+input = (lambda: sys.stdin.readline().rstrip("\r\n"))
+
+
+YES = "Yes"  # type: str
+NO = "No"  # type: str
+
+
+def chmax(a: Any, b: Any) -> Tuple[Any, bool]:
+    if (a < b):
+        a = b  # aをbで更新
+        return (a, True)
+    return (a, False)
+
+
+def chmin(a: Any, b: Any) -> Tuple[Any, bool]:
+    if (a > b):
+        a = b  # aをbで更新
+        return (a, True)
+    return (a, False)
+
+
+def solve(S: str):
+    S_l = len(S)
+
+    r = S_l-1
+    l = 0
+    while l < r:
+        if S[l] == S[r]:
+            l += 1
+            r -= 1
+        else:
+            if S[r] == 'a':
+                r -= 1
+            else:
+                break
+
+    if l >= r:
+        print(YES)
+    else:
+        print(NO)
+    return
+
+
+def main():
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+    tokens = iterate_tokens()
+    S = next(tokens)  # type: str
+    solve(S)
+
+
+if __name__ == '__main__':
+    main()
