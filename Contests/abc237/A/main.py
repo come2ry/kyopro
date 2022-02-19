da8b348749dc4c7acb8caa2d9a261d23523b158d
come2ry <raiki5100@gmail.com>
2022-02-19 20:26:11 +0900

    AC: abc237_A

---
+++ Contests/abc237/A/main.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# https://atcoder.jp/contests/abc237/tasks/abc237_A?lang=ja
+import bisect  # noqa: F401
+import heapq  # noqa: F401
+import math  # noqa: F401
+import sys  # noqa: F401
+from collections import defaultdict, deque  # noqa: F401
+from functools import lru_cache  # noqa: F401
+from itertools import (accumulate, combinations,  # noqa: F401
+                       combinations_with_replacement, groupby, permutations,
+                       product)
+from typing import *  # noqa: F401
+
+input = (lambda: sys.stdin.readline().rstrip("\r\n"))
+
+
+YES = "Yes"  # type: str
+NO = "No"  # type: str
+
+
+def chmax(a: Any, b: Any) -> Tuple[Any, bool]:
+    if (a < b):
+        a = b  # aをbで更新
+        return (a, True)
+    return (a, False)
+
+
+def chmin(a: Any, b: Any) -> Tuple[Any, bool]:
+    if (a > b):
+        a = b  # aをbで更新
+        return (a, True)
+    return (a, False)
+
+
+def solve(N: int):
+    t = 2**31
+    if N >= -t and N < t:
+        print(YES)
+    else:
+        print(NO)
+    return
+
+
+def main():
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+    tokens = iterate_tokens()
+    N = int(next(tokens))  # type: int
+    solve(N)
+
+
+if __name__ == '__main__':
+    main()
