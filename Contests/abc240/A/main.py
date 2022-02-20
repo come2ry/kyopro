4fed0e4aeb6982f242b9e2cb8d13e47c5d640c18
come2ry <raiki5100@gmail.com>
2022-02-21 01:13:35 +0900

    AC: abc240_A

---
+++ Contests/abc240/A/main.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# https://atcoder.jp/contests/abc240/tasks/abc240_A?lang=ja
+import bisect  # noqa: F401
+import heapq  # noqa: F401
+import math  # noqa: F401
+import sys  # noqa: F401
+from collections import defaultdict, deque  # noqa: F401
+from functools import lru_cache  # noqa: F401
+from itertools import (accumulate, combinations,  # noqa: F401
+                       combinations_with_replacement, groupby, permutations,
+                       product)
+from typing import *  # noqa: F401
+
+input = (lambda: sys.stdin.readline().rstrip("\r\n"))
+
+
+YES = "Yes"  # type: str
+NO = "No"  # type: str
+
+
+def chmax(a: Any, b: Any) -> Tuple[Any, bool]:
+    if (a < b):
+        a = b  # aをbで更新
+        return (a, True)
+    return (a, False)
+
+
+def chmin(a: Any, b: Any) -> Tuple[Any, bool]:
+    if (a > b):
+        a = b  # aをbで更新
+        return (a, True)
+    return (a, False)
+
+
+def solve(a: int, b: int):
+
+    if a == 1 and b == 10:
+        print(YES)
+        return
+    else:
+        if b - a == 1:
+            print(YES)
+            return
+
+    print(NO)
+    return
+
+
+def main():
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+    tokens = iterate_tokens()
+    a = int(next(tokens))  # type: int
+    b = int(next(tokens))  # type: int
+    solve(a, b)
+
+
+if __name__ == '__main__':
+    main()
