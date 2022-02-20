c92d2cbdca98e43e00319ecb8a57d545fdc2179c
come2ry <raiki5100@gmail.com>
2022-02-21 01:13:36 +0900

    AC: abc240_B

---
+++ Contests/abc240/B/main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# https://atcoder.jp/contests/abc240/tasks/abc240_B?lang=ja
+import bisect  # noqa: F401
+import heapq  # noqa: F401
+import math  # noqa: F401
+import sys  # noqa: F401
+from collections import defaultdict, deque  # noqa: F401
+from functools import lru_cache  # noqa: F401
+from itertools import (accumulate, combinations,  # noqa: F401
+                       combinations_with_replacement, groupby, permutations,
+                       product)
+from typing import *  # noqa: F401
+
+input = (lambda: sys.stdin.readline().rstrip("\r\n"))
+
+
+def chmax(a: Any, b: Any) -> Tuple[Any, bool]:
+    if (a < b):
+        a = b  # aをbで更新
+        return (a, True)
+    return (a, False)
+
+
+def chmin(a: Any, b: Any) -> Tuple[Any, bool]:
+    if (a > b):
+        a = b  # aをbで更新
+        return (a, True)
+    return (a, False)
+
+
+def solve(N: int, a: "List[int]"):
+    print(len(set(a)))
+    return
+
+
+def main():
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+    tokens = iterate_tokens()
+    N = int(next(tokens))  # type: int
+    a = [int(next(tokens)) for _ in range(N)]  # type: "List[int]"
+    solve(N, a)
+
+
+if __name__ == '__main__':
+    main()
