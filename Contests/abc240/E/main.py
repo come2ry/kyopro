70d8e89a7d87478ec9f71ebf3ec8f3f03e788304
come2ry <raiki5100@gmail.com>
2022-02-21 01:27:14 +0900

    AC: abc240_E

--- Contests/abc240/E/main.py
+++ Contests/abc240/E/main.py
@@ -10,8 +10,10 @@ from itertools import (accumulate, combinations,  # noqa: F401
                        combinations_with_replacement, groupby, permutations,
                        product)
 from typing import *  # noqa: F401
-# import pypyjit
-# pypyjit.set_param('max_unroll_recursion=-1')
+sys.setrecursionlimit(10**7)
+if "PyPy" in sys.version:
+    import pypyjit
+    pypyjit.set_param('max_unroll_recursion=-1')
 
 input = (lambda: sys.stdin.readline().rstrip("\r\n"))
 
@@ -56,48 +58,6 @@ def dfs(node_id, parent_id):
     return
 
 
-# @lru_cache(maxsize=10**6)
-# def check_include(x, y):
-#     if x == y:
-#         return True
-
-#     for c in edges[y]:
-#         if c == parents[y]:
-#             continue
-#         if check_include(x, c):
-#             return True
-
-#     return False
-
-
-# def judge(N):
-#     global ans
-
-#     for i, a in enumerate(ans):
-#         assert (a[0] <= a[1]), f"{i}: L({a[0]}) > R({a[1]})"
-
-#     for i in range(N):
-#         for j in range(i, N):
-#             if i == j:
-#                 continue
-
-#             if check_include(i, j):
-#                 assert ((ans[i][0] >= ans[j][0]) and (ans[i][1] <= ans[j][1])
-#                         ), f"{i}: S_{i} \\in S_{j}なのに [Li({ans[i][0]}), Ri({ans[i][1]})] not \\in [Lj({ans[j][0]}), Rj({ans[j][1]})]"
-
-#             elif check_include(j, i):
-#                 i, j = j, i
-#                 assert ((ans[i][0] >= ans[j][0]) and (ans[i][1] <= ans[j][1])
-#                         ), f"{i}: S_{i} \\in S_{j}なのに [Li({ans[i][0]}), Ri({ans[i][1]})] not \\in [Lj({ans[j][0]}), Rj({ans[j][1]})]"
-
-#             else:
-#                 intersect_set = set(range(ans[i][0], ans[i][1] + 1)) & set(range(ans[j][0], ans[j][1] + 1))
-#                 assert (
-# len(intersect_set) == 0), f"{i}: S_{i} \\and S_{j} = \\phi なのに
-# [Li({ans[i][0]}), Ri({ans[i][1]})] \\and [Lj({ans[j][0]}),
-# Rj({ans[j][1]})] != \\phi({intersect_set})"
-
-
 def solve(N: int, u: "List[int]", v: "List[int]"):
     global edges, ans, parents
     edges = [[] for _ in range(N)]
@@ -135,7 +95,6 @@ def main():
         u[i] = int(next(tokens))
         v[i] = int(next(tokens))
     solve(N, u, v)
-    # judge(N)
 
 
 if __name__ == '__main__':
