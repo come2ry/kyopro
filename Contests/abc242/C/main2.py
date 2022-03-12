e6f6fe07af827fa8a14e6d5a5c3d465f6423df1d
come2ry <raiki5100@gmail.com>
2022-03-12 16:30:21 +0900

    AC: abc242_C 正攻法

--- Contests/abc242/C/main2.py
+++ Contests/abc242/C/main2.py
@@ -36,51 +36,30 @@ def chmin(a: Any, b: Any) -> Tuple[Any, bool]:
 
 
 def solve(N: int):
-    ans = deque(list(range(9)))
-    for i in range(1, N):
-        l = len(ans)
-        for j in range(l):
-            a = ans.popleft()
+    K = 9
+    dp = [[0] * (K + 1) for _ in range(N + 1)]
 
-            a_str = str(a)
-            for next_a in range(max(0, int(a_str[-1]) - 1), min(8, int(a_str[-1]) + 1) + 1):
-                ans.append(a * 10 + next_a)
+    for j in range(1, K + 1):
+        dp[1][j] = 1
 
-    print(len(ans))
+    for i in range(2, N + 1):
+        for j in range(1, K + 1):
+            for k in range(max(1, j - 1), min(K + 1, j + 2)):
+                dp[i][j] += dp[i - 1][k]
+                dp[i][j] %= MOD
+
+    print(sum(dp[N]) % MOD)
     return
 
 
-# @lru_cache(maxsize=1000000)
-def T(k):
-    if k == 1:
-        return 8 + 1
-    elif k == 2:
-        return 3 * 8 + 1
-    elif k == 3:
-        return 9 * 8 - 1
-    elif k == 4:
-        return 27 * 8 - 13
-    elif k == 5:
-        return 81 * 8 - 65
-    elif k == 6:
-        return 243 * 8 - 265
-    elif k == 7:
-        return 729 * 8 - 987
-    elif k == 8:
-        return 2187 * 8 - 3495
-
-    return 5 * T(k - 1) - 5 * T(k - 2) - 5 * T(k - 3) + 5 * T(k - 4) + T(k - 5)
-
-
 def main():
-    # def iterate_tokens():
-    #     for line in sys.stdin:
-    #         for word in line.split():
-    #             yield word
-    # tokens = iterate_tokens()
-    # N = int(next(tokens))  # type: int
-    for i in range(2, 10):
-        print(T(i))
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+    tokens = iterate_tokens()
+    N = int(next(tokens))  # type: int
+    solve(N)
 
 
 if __name__ == '__main__':
