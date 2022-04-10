37847fec3742fc732a5c4e90519486d0a421ba93
come2ry <raiki5100@gmail.com>
2022-04-10 23:20:19 +0900

    AC: abc247_C

---
+++ Contests/abc247/C/main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# https://atcoder.jp/contests/abc247/tasks/abc247_C?lang=ja
+import bisect  # noqa: F401
+import heapq  # noqa: F401
+import math  # noqa: F401
+import sys  # noqa: F401
+from collections import defaultdict, deque  # noqa: F401
+from functools import lru_cache  # noqa: F401
+from itertools import (accumulate, combinations,  # noqa: F401
+                       combinations_with_replacement, groupby, permutations,
+                       product)
+from typing import *  # noqa: F401
+sys.setrecursionlimit(10**7)
+if "PyPy" in sys.version:
+    import pypyjit
+    pypyjit.set_param('max_unroll_recursion=-1')
+
+input = (lambda: sys.stdin.readline().rstrip("\r\n"))
+
+
+def chmax(a: Any, b: Any) -> Tuple[Any, bool]:
+    if (a < b):
+        a = b  # aをbで更新
+        return (a, True)
+    return (a, False)
+
+
+def chmin(a: Any, b: Any) -> Tuple[Any, bool]:
+    if (a > b):
+        a = b  # aをbで更新
+        return (a, True)
+    return (a, False)
+
+
+@lru_cache(maxsize=10**6)
+def f(n):
+    if n == 1:
+        return tuple([1])
+    return f(n - 1) + tuple([n]) + f(n - 1)
+
+
+def solve(N: int):
+    print(' '.join(map(str, f(N))))
+    return
+
+
+def main():
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+    tokens = iterate_tokens()
+    N = int(next(tokens))  # type: int
+    solve(N)
+
+
+if __name__ == '__main__':
+    main()
