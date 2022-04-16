5458f1fcad01c6f40cad7f94bc953bb8d583fdea
come2ry <raiki5100@gmail.com>
2022-04-16 15:11:32 +0900

    AC: abc247_E

--- Contests/abc247/E/main.py
+++ Contests/abc247/E/main.py
@@ -32,114 +32,63 @@ def chmin(a: Any, b: Any) -> Tuple[Any, bool]:
     return (a, False)
 
 
-class SegTree:
-    """
-    init(init_val, ide_ele): 配列init_valで初期化 O(N)
-    update(k, x): k番目の値をxに更新 O(N)
-    query(l, r): 区間[l, r)をsegfuncしたものを返す O(logN)
-    """
+def solve_child(query, X, Y):
+    x_count_list = [0] * len(query)
+    y_count_list = [0] * len(query)
 
-    def __init__(self, init_val, segfunc, ide_ele):
-        """
-        init_val: 配列の初期値
-        segfunc: 区間にしたい操作
-        ide_ele: 単位元
-        n: 要素数
-        num: n以上の最小の2のべき乗
-        tree: セグメント木(1-index)
-        """
-        n = len(init_val)
-        self.segfunc = segfunc
-        self.ide_ele = ide_ele
-        self.num = 1 << (n - 1).bit_length()
-        self.tree = [ide_ele] * 2 * self.num
-        # 配列の値を葉にセット
-        for i in range(n):
-            self.tree[self.num + i] = init_val[i]
-        # 構築していく
-        for i in range(self.num - 1, 0, -1):
-            self.tree[i] = self.segfunc(self.tree[2 * i], self.tree[2 * i + 1])
+    for i in range(len(query)):
+        if query[i] == X:
+            x_count_list[i] = 1
+        if query[i] == Y:
+            y_count_list[i] = 1
 
-    def update(self, k, x):
-        """
-        k番目の値をxに更新
-        k: index(0-index)
-        x: update value
-        """
-        k += self.num
-        self.tree[k] = x
-        while k > 1:
-            self.tree[k >> 1] = self.segfunc(self.tree[k], self.tree[k ^ 1])
-            k >>= 1
+    x_count_list = list(accumulate(x_count_list))
+    y_count_list = list(accumulate(y_count_list))
 
-    def query(self, l, r):
-        """
-        [l, r)のsegfuncしたものを得る
-        l: index(0-index)
-        r: index(0-index)
-        """
-        res = self.ide_ele
+    # print(query, X, Y)
+    # print(x_count_list)
+    # print(y_count_list)
+    # print()
 
-        l += self.num
-        r += self.num
-        while l < r:
-            if l & 1:
-                res = self.segfunc(res, self.tree[l])
-                l += 1
-            if r & 1:
-                res = self.segfunc(res, self.tree[r - 1])
-            l >>= 1
-            r >>= 1
-        return res
+    ans = 0
+    for i in range(len(query)):
+        x_search = 1
+        y_search = 1
+        if i != 0:
+            x_search = x_count_list[i - 1] + 1
+            y_search = y_count_list[i - 1] + 1
+
+        x_index = bisect.bisect_left(x_count_list[i:], x_search)
+        y_index = bisect.bisect_left(y_count_list[i:], y_search)
+        # print(x_count_list[i:], x_search, x_index)
+        # print(y_count_list[i:], y_search, y_index)
+
+        if x_index == len(query):
+            break
+        if y_index == len(query):
+            break
+
+        # print(len(query) - i - max(x_index, y_index))
+        ans += (len(query) - i - max(x_index, y_index))
+
+    return ans
 
 
 def solve(N: int, X: int, Y: int, A: "List[int]"):
-    def min_segfunc(x, y):
-        x, _ = chmin(x, y)
-        return x
-
-    def max_segfunc(x, y):
-        x, _ = chmax(x, y)
-        return x
-
-    ide_ele = float('inf')
-    min_seg = SegTree(A, min_segfunc, ide_ele)
-    max_seg = SegTree(A, max_segfunc, -ide_ele)
-
+    i = 0
     ans = 0
-    r = 1
-    # 次のrが行けるか確認
-    min_a, max_a = float('inf'), -float('inf')
-    for l in range(N):
-        while (r <= N):
-            print(f"{l}, {r}")
-            if l >= r:
-                r += 1
-                print("r+=1")
+    while i < N:
+        query = []
+        while i < N:
+            if (Y <= A[i] <= X):
+                query.append(A[i])
+                i += 1
                 continue
-
-            # 次確認
-            min_a = min_seg.query(l, r)
-            max_a = max_seg.query(l, r)
-            print(f"({min_a}, {max_a})")
-
-            # ansなら
-            if min_a == Y and max_a == X:
-                ans += l
-                print(f"ans: {ans}")
-
             else:
-                if (min_a < Y) or (max_a > X):
-                    print(f"{l}, {r}, x")
-                    break
-            # 次へ
-            if r < N:
-                r += 1
-                print("r+=1")
-            else:
+                i += 1
                 break
-
-        print("l+=1")
+        if len(query):
+            ans += solve_child(query, X, Y)
 
     print(ans)
     return
