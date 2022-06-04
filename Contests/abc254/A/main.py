a6421a25cd2fe8cd01c0915eb0a335bb969cd1d5
come2ry <raiki5100@gmail.com>
2022-06-04 22:49:44 +0900

    AC: abc254_A

---
+++ Contests/abc254/A/main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# https://atcoder.jp/contests/abc254/tasks/abc254_A?lang=ja
+import bisect  # noqa: F401
+import heapq  # noqa: F401
+import math  # noqa: F401
+import sys  # noqa: F401
+from collections import defaultdict, deque  # noqa: F401
+from functools import lru_cache  # noqa: F401
+from itertools import (accumulate, combinations,  # noqa: F401
+                       combinations_with_replacement, groupby, permutations,
+                       product)
+from typing import *  # noqa: F401
+sys.setrecursionlimit(10**7)
+if "PyPy" in sys.version:
+    import pypyjit
+    pypyjit.set_param('max_unroll_recursion=-1')
+
+input = (lambda: sys.stdin.readline().rstrip("\r\n"))
+
+
+def solve(N: int):
+    print(str(N)[-2:])
+    return
+
+
+def main():
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+    tokens = iterate_tokens()
+    N = int(next(tokens))  # type: int
+    solve(N)
+
+
+if __name__ == '__main__':
+    main()
