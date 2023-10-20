f37024ede8514585edce3ddca7d5bbc5b5fed56b
come2ry <raiki5100@gmail.com>
2023-10-20 22:59:31 +0900

    AC: abc324_C

--- Contests/abc324/C/main.py
+++ Contests/abc324/C/main.py
@@ -23,35 +23,105 @@ def main():
     N, Td = input().split()
     N = int(N)
 
-    # 1. T′は、Tと等しい。
-    T_candidates = set(Td)
-    for i in range(len(Td)):
-        # 2. T′は、Tのいずれか1つの位置（先頭と末尾も含む）に英小文字を1つ挿入して得られる文字列である。
-        c = list(Td)
-        c.pop(i)
-        T_candidates.add("".join(c))
+    # # 1. T′は、Tと等しい。
+    # T_candidates = set(Td)
+    # for i in range(len(Td)):
+    #     # 2. T′は、Tのいずれか1つの位置（先頭と末尾も含む）に英小文字を1つ挿入して得られる文字列である。
+    #     c = list(Td)
+    #     c.pop(i)
+    #     T_candidates.add("".join(c))
 
-    for i in range(len(Td) + 1):
-        # T′は、Tからある1文字を削除して得られる文字列である。
-        for s in string.ascii_lowercase:
-            c = list(Td)
-            c.insert(i, s)
-            T_candidates.add("".join(c))
+    # for i in range(len(Td) + 1):
+    #     # T′は、Tからある1文字を削除して得られる文字列である。
+    #     for s in string.ascii_lowercase:
+    #         c = list(Td)
+    #         c.insert(i, s)
+    #         T_candidates.add("".join(c))
 
-    for i in range(len(Td)):
-        # T′は、Tのある1文字を別の英小文字に変更して得られる文字列である。
-        for s in string.ascii_lowercase:
-            c = list(Td)
-            c[i] = s
-            T_candidates.add("".join(c))
+    # for i in range(len(Td)):
+    #     # T′は、Tのある1文字を別の英小文字に変更して得られる文字列である。
+    #     for s in string.ascii_lowercase:
+    #         c = list(Td)
+    #         c[i] = s
+    #         T_candidates.add("".join(c))
 
     result = []
     for i in range(N):
         S = input()
+        # print(i, f"{Td},{S}: ")
         if len(S) < len(Td) - 1 or len(S) > len(Td) + 1:
+            # print("skip(len)")
             continue
-        if S in T_candidates:
+
+        if len(S) == len(Td):
+            if S == Td:
+                result.append(i + 1)
+                # print("ok(==,0diff)")
+                continue
+            d = 0
+            for j in range(len(S)):
+                if S[j] != Td[j]:
+                    d += 1
+                    if d >= 2:
+                        break
+            if d >= 2:
+                # print("skip(==,2diff)")
+                continue
+
+            # print("ok(==,1diff)")
             result.append(i + 1)
+
+        elif len(S) == len(Td) + 1:
+            l = 0
+            r = 0
+            while l < len(S) and r < len(Td):
+                if S[l] != Td[r]:
+                    if l == r:
+                        l += 1
+                        continue
+                    else:
+                        l += 1
+                        break
+                l += 1
+                r += 1
+
+            if (l - r) == 0:
+                # print("ok(S>Td,1diff)")
+                result.append(i + 1)
+            if (l - r) == 1:
+                # print("ok(S>Td,1diff)")
+                result.append(i + 1)
+            else:
+                pass
+                # print("skip(S>Td,2diff)")
+
+        elif len(S) == len(Td) - 1:
+            l = 0
+            r = 0
+            while l < len(S) and r < len(Td):
+                if S[l] != Td[r]:
+                    if l == r:
+                        r += 1
+                        continue
+                    else:
+                        r += 1
+                        break
+                l += 1
+                r += 1
+
+            if (r - l) == 0:
+                # print("ok(S<Td,1diff)")
+                result.append(i + 1)
+            if (r - l) == 1:
+                # print("ok(S<Td,1diff)")
+                result.append(i + 1)
+            else:
+                # print("skip(S<Td,2diff)")
+                pass
+
+        else:
+            # print("skip(else)")
+            pass
 
     print(len(result))
     if len(result):
